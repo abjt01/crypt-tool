3c0b3fbe194dfc826de6c10ef6a9fed0146ff3d9
abjt01 <abjty02@gmail.com>
2026-03-28 01:14:03 +0530

    add password prompt

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
-)
+	"strconv"
+	"strings"
 
-func main() {
-	fmt.Println("crypt-tool")
-}
-package main
+	"crypt-tool/filecrypt"
 
-import (
-	"fmt"
-	"os"
+	"golang.org/x/term"
 )
 
 func main() {
@@ -22,10 +19,34 @@ func main() {
 	}
 
 	action := os.Args[1]
+	mode := os.Args[2]
 
 	switch action {
 	case "help":
 		printHelp()
+
+	case "encrypt":
+		switch mode {
+		case "text":
+			handleTextEncrypt()
+		case "file":
+			handleFileEncrypt()
+		default:
+			fmt.Println("Mode must be 'text' or 'file'.")
+			os.Exit(1)
+		}
+
+	case "decrypt":
+		switch mode {
+		case "text":
+			handleTextDecrypt()
+		case "file":
+			handleFileDecrypt()
+		default:
+			fmt.Println("Mode must be 'text' or 'file'.")
+			os.Exit(1)
+		}
+
 	default:
 		printHelp()
 		os.Exit(1)
@@ -47,6 +68,7 @@ Examples:
   go run . encrypt file test.jpeg
   go run . decrypt file test.jpeg`)
 }
+
 // text(simple letter shift cipher)
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -70,6 +92,7 @@ func encryptText(key int, plain string) string {
 	}
 	return sb.String()
 }
+
 func decryptText(key int, enc string) string {
 	shifted := shiftAlphabet(key)
 	var sb strings.Builder
@@ -84,8 +107,61 @@ func decryptText(key int, enc string) string {
 	return sb.String()
 }
 
-case "encrypt":
-	switch mode {
-	case "file":
-		handleFileEncrypt()
+func handleTextEncrypt() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: go run . encrypt text \"TEXT\" <key>")
+		os.Exit(1)
 	}
+	key, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Println("Key must be an integer.")
+		os.Exit(1)
+	}
+	fmt.Println("Encrypted:", encryptText(key, os.Args[3]))
+}
+
+func handleTextDecrypt() {
+	if len(os.Args) < 5 {
+		fmt.Println("Usage: go run . decrypt text \"TEXT\" <key>")
+		os.Exit(1)
+	}
+	key, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Println("Key must be an integer.")
+		os.Exit(1)
+	}
+	fmt.Println("Decrypted:", decryptText(key, os.Args[3]))
+}
+
+func handleFileEncrypt() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run . encrypt file <path>")
+		os.Exit(1)
+	}
+	f := os.Args[3]
+	filecrypt.Encrypt(f, []byte("password"))
+	fmt.Println("File encrypted successfully.")
+}
+
+func handleFileDecrypt() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run . decrypt file <path>")
+		os.Exit(1)
+	}
+	f := os.Args[3]
+	filecrypt.Decrypt(f, []byte("password"))
+	fmt.Println("File decrypted successfully.")
+}
+
+func getPassword() []byte {
+	fmt.Print("Enter password: ")
+	p1, _ := term.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Print("\nConfirm password: ")
+	p2, _ := term.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	if !bytes.Equal(p1, p2) {
+		fmt.Println("Passwords don't match, try again.")
+		return getPassword()
+	}
+	return p1
+}
