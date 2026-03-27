521c3a02186dd2d389a0410e927f3ef96ab2eb34
abjt01 <abjty02@gmail.com>
2026-03-28 01:10:19 +0530

    add help command

--- main.go
+++ main.go
@@ -2,8 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("crypt-tool")
+}
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		printHelp()
+		os.Exit(0)
+	}
+
+	action := os.Args[1]
+
+	switch action {
+	case "help":
+		printHelp()
+	default:
+		printHelp()
+		os.Exit(1)
+	}
+}
+
+func printHelp() {
+	fmt.Println(`crypt-tool — text & file encryption
+
+Usage:
+  go run . encrypt text "YOUR TEXT" <key>
+  go run . decrypt text "ENCRYPTED"  <key>
+  go run . encrypt file <path>
+  go run . decrypt file <path>
+
+Examples:
+  go run . encrypt text "HELLO WORLD" 5
+  go run . decrypt text "MJQQT BTWQI" 5
+  go run . encrypt file test.jpeg
+  go run . decrypt file test.jpeg`)
 }
